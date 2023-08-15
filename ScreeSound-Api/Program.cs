bb7247b8a0fac3128c342ba0d8fe1701208718e2
Lucas Vinicius <lucas_vini_eu@hotmail.com>
2023-08-14 23:39:35 -0300

    Projeto inicial acessando API

---
+++ ScreeSound-Api/Program.cs
@@ -0,0 +1,5 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    string resposta = await client.GetStringAsync("http://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+}
