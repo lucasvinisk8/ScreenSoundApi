7540f8da4fb74c432a4d0de0e860440c1df671a2
Lucas Vinicius <lucas_vini_eu@hotmail.com>
2023-08-15 00:24:28 -0300

    Deserializando json request da API

---
+++ ScreeSound-Api/Modelos/Musica.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace ScreeSound_Api.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
+        Console.WriteLine($"Gênero: {Genero}");
+    }
+}
--- ScreeSound-Api/Program.cs
+++ ScreeSound-Api/Program.cs
@@ -1,5 +1,16 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreeSound_Api.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
+    try
+    {
     string resposta = await client.GetStringAsync("http://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[25].ExibirDetalhesDaMusica();
+    }
+    catch (Exception ex) 
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
 }
